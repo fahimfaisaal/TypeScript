fe6861890d89668ce23a70a25319f20934ff10cf
Fahim Faisal <fahimfaisal.soikot@gmail.com>
2021-11-10 19:16:37 +0600

    feat: add enums

---
+++ src/Enums/lengthUnit.ts
@@ -0,0 +1,12 @@
+enum Length {
+  Millimeter = "mm",
+  Centimeter = "cm",
+  Meter = "m",
+  Kilometer = "km",
+  Inch = "in",
+  Foot = "ft",
+  Yard = "yd",
+  Mile = "mi",
+}
+
+export default Length;
---
+++ src/Enums/weightUnit.ts
@@ -0,0 +1,8 @@
+enum Weight {
+  Gram = "g",
+  Kilogram = "kg",
+  Ounce = "oz",
+  Pound = "lb",
+}
+
+export default Weight;
